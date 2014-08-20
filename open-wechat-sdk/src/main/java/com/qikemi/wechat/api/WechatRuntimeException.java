71b7776809cc0fa5d8ef38f89f2e034e9a11df03
Xie Xianbin <xianbinxie@163.com>
2014-08-20 22:26:37 +0800

    add open-wechat-sdk RuntimetimeException

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/WechatRuntimeException.java
@@ -0,0 +1,28 @@
+package com.qikemi.wechat.api;
+
+/**
+ * 
+ * Create Date: 2014年8月20日 下午9:54:46
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class WechatRuntimeException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+    public WechatRuntimeException(){
+        super();
+    }
+
+    public WechatRuntimeException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public WechatRuntimeException(String message){
+        super(message);
+    }
+
+    public WechatRuntimeException(Throwable cause){
+        super(cause);
+    }
+}
