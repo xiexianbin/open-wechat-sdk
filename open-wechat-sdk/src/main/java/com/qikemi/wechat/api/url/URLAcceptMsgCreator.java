d41a7836079b8acecc04b1b2bf6253bdd4e6f63a
Xie Xianbin <xianbinxie@163.com>
2014-09-19 23:44:34 +0800

    add URLAcceptMsgCreator

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/url/URLAcceptMsgCreator.java
@@ -0,0 +1,11 @@
+package com.qikemi.wechat.api.url;
+
+/**
+ * 
+ * Create Date: 2014年9月19日 下午11:01:14
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class URLAcceptMsgCreator {
+
+}
