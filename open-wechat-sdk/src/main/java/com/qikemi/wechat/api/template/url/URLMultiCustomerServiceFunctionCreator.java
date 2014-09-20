6e5b893d4a004c8883a3e13174cb0312f4ea629a
Xie Xianbin <xianbinxie@163.com>
2014-09-20 13:18:26 +0800

    add URLMultiCustomerServiceFuncCreator

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/template/url/URLMultiCustomerServiceFunctionCreator.java
@@ -0,0 +1,11 @@
+package com.qikemi.wechat.api.template.url;
+
+/**
+ * 
+ * Create Date: 2014年9月19日 下午11:52:11
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class URLMultiCustomerServiceFunctionCreator {
+
+}
