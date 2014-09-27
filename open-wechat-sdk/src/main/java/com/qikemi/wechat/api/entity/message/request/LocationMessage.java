6b6d58582221355152825c4e95d2f5319ed5eb93
Xie Xianbin <xianbinxie@163.com>
2014-09-27 22:24:34 +0800

    add location message bean

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/entity/message/request/LocationMessage.java
@@ -0,0 +1,102 @@
+package com.qikemi.wechat.api.entity.message.request;
+
+import com.qikemi.wechat.api.entity.message.RequestBaseMessage;
+
+/**
+ * Location Message bean
+ * Create Date: 2014年9月27日 下午10:23:51
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class LocationMessage extends RequestBaseMessage {
+	
+	/**
+	 * 地理位置维度
+	 */
+	private String Location_X;
+	
+	/**
+	 * 地理位置经度
+	 */
+	private String Location_Y;
+
+	/**
+	 * 地图缩放大小
+	 */
+	private String Scale;
+
+	/**
+	 * 地理位置信息
+	 */
+	private String Label;
+
+	/**             constructor                  **/
+	public LocationMessage() {
+		super();
+	}
+
+	/**
+	 * @param location_X
+	 * @param location_Y
+	 * @param scale
+	 * @param label
+	 */
+	public LocationMessage(String location_X, String location_Y, String scale,
+			String label) {
+		super();
+		Location_X = location_X;
+		Location_Y = location_Y;
+		Scale = scale;
+		Label = label;
+	}
+
+	/**
+	 * @param location_X
+	 * @param location_Y
+	 * @param scale
+	 * @param label
+	 */
+	public LocationMessage(String toUserName, String fromUserName, long createTime, 
+			String msgType, long msgId, String location_X, String location_Y, String scale,
+			String label) {
+		super(toUserName, fromUserName, createTime, msgType, msgId);
+		Location_X = location_X;
+		Location_Y = location_Y;
+		Scale = scale;
+		Label = label;
+	}
+
+	/**           getters and setters            **/
+	public String getLocation_X() {
+		return Location_X;
+	}
+
+	public void setLocation_X(String location_X) {
+		Location_X = location_X;
+	}
+
+	public String getLocation_Y() {
+		return Location_Y;
+	}
+
+	public void setLocation_Y(String location_Y) {
+		Location_Y = location_Y;
+	}
+
+	public String getScale() {
+		return Scale;
+	}
+
+	public void setScale(String scale) {
+		Scale = scale;
+	}
+
+	public String getLabel() {
+		return Label;
+	}
+
+	public void setLabel(String label) {
+		Label = label;
+	}
+	
+}
