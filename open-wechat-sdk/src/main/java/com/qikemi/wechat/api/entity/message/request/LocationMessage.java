d5858c51653e04c8c68d7e9ad2366bc3801480dc
Xie Xianbin <xianbinxie@163.com>
2014-09-27 22:27:24 +0800

    update

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/entity/message/request/LocationMessage.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/entity/message/request/LocationMessage.java
@@ -98,5 +98,17 @@ public class LocationMessage extends RequestBaseMessage {
 	public void setLabel(String label) {
 		Label = label;
 	}
+
+	@Override
+	public String toString() {
+		return "LocationMessage [Location_X=" + Location_X + ", Location_Y="
+				+ Location_Y + ", Scale=" + Scale + ", Label=" + Label
+				+ ", getToUserName()=" + getToUserName()
+				+ ", getFromUserName()=" + getFromUserName()
+				+ ", getCreateTime()=" + getCreateTime() + ", getMsgType()="
+				+ getMsgType() + ", getMsgId()=" + getMsgId() + ", toString()="
+				+ super.toString() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + "]";
+	}
 	
 }
