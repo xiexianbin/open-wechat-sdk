074cb87f6b6f888a76563ce2b18d26e1bbbff2cf
Xie Xianbin <xianbinxie@163.com>
2014-09-27 21:47:13 +0800

    wechat event type constant

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/constant/WechatEventTypeConstant.java
@@ -0,0 +1,25 @@
+package com.qikemi.wechat.api.constant;
+
+/**
+ * WECHAT Event Type Constant 
+ * Create Date: 2014年9月27日 下午9:39:22
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class WechatEventTypeConstant {
+
+	/**
+	 * 事件类型：subscribe(关注)
+	 */
+	public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
+	
+	/**
+	 * 事件类型：unSubscribe(取消关注)
+	 */
+	public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
+	
+	/**
+	 * 事件类型：CLICK(自定义菜单点击事件)
+	 */
+	public static final String EVENT_TYPE_CLICK = "CLICK";
+}
