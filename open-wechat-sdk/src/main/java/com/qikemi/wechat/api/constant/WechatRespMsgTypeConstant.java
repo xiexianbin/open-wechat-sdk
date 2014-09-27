a2acfe2c42596f57516f06327f950813a24bacb9
Xie Xianbin <xianbinxie@163.com>
2014-09-27 21:46:52 +0800

    wechat Type Constant

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/constant/WechatRespMsgTypeConstant.java
@@ -0,0 +1,40 @@
+package com.qikemi.wechat.api.constant;
+
+/**
+ * WECHAT RESPONSE Type Constant 
+ * Create Date: 2014年9月27日 下午9:44:08
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class WechatRespMsgTypeConstant {
+	
+	/**
+	 * 返回消息类型：文本
+	 */
+	public static final String RESP_MSG_TYPE_TEXT = "text";
+	
+	/**
+	 * 返回消息类型：图片
+	 */
+	public static final String RESP_MSG_TYPE_IMAGE = "image";
+	
+	/**
+	 * 返回消息类型：语音
+	 */
+	public static final String RESP_MSG_TYPE_VOICE = "voice";
+	
+	/**
+	 * 返回消息类型：视频
+	 */
+	public static final String RESP_MSG_TYPE_VIDEO = "video";
+	
+	/**
+	 * 返回消息类型：音乐
+	 */
+	public static final String RESP_MSG_TYPE_MUSIC = "music";
+	
+	/**
+	 * 返回消息类型：图文
+	 */
+	public static final String RESP_MSG_TYPE_NEWS = "news";
+}
