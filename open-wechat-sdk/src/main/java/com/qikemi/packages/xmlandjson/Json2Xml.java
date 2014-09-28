c38459ea8fad0d975185d17dc37926e682fac14b
Xie Xianbin <xianbinxie@163.com>
2014-09-28 20:47:52 +0800

    add json2xml

---
+++ open-wechat-sdk/src/main/java/com/qikemi/packages/xmlandjson/Json2Xml.java
@@ -0,0 +1,11 @@
+package com.qikemi.packages.xmlandjson;
+
+/**
+ * 
+ * Create Date: 2014年9月28日 下午8:04:42
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class Json2Xml {
+
+}
