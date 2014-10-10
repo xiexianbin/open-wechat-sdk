358ae512267b732f450eeeb326687e7baf947716
xiexianbin <a.b@hotmail.com>
2014-10-10 23:22:17 +0800

    doget

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/servlet/WechatServlet.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/servlet/WechatServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.qikemi.wechat.api.service.SignService;
+
 /**
  * 
  * Create Date: 2014年10月10日 下午9:42:26 Author XieXianbin<a.b@hotmail.com> Source
@@ -24,10 +26,18 @@ public class WechatServlet extends HttpServlet {
 		req.setCharacterEncoding("utf-8");
 		resp.setCharacterEncoding("utf-8");
 
-		PrintWriter out = resp.getWriter();
-		out.println("Hello, Brave new World!");
+		// 随机字符串
+		String echostr = req.getParameter("echostr");
+
+		PrintWriter out = null;
+		out = resp.getWriter();
+		// 通过检验signature对请求进行校验，若校验成功则原样返回echostr，否则接入失败
+		if (SignService.checkSignature(req)) {
+			out.print(echostr);
+		}else{
+			out.print("Error Configure.");
+		}
 		out.close();
-		super.doGet(req, resp);
 	}
 
 	@Override
