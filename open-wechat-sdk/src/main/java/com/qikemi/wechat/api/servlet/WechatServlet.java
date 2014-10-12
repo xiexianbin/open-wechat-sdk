88a026161469c3b318a753dcefdc30979e2af2cf
xiexianbin <a.b@hotmail.com>
2014-10-12 20:11:05 +0800

    update

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/servlet/WechatServlet.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/servlet/WechatServlet.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import com.qikemi.wechat.api.constant.WechatEventTypeConstant;
+import com.qikemi.wechat.api.constant.WechatReqMsgTypeConstant;
+import com.qikemi.wechat.api.entity.message.MsgTypeBean;
+import com.qikemi.wechat.api.service.MsgTypeService;
 import com.qikemi.wechat.api.service.SignService;
 
 /**
@@ -56,8 +60,91 @@ public class WechatServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
+		// 处理编码 
+		try {
+			req.setCharacterEncoding("utf-8");
+			resp.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			logger.error("req编码错误：" + e1.getMessage());
+		}
+		// 接受信息 
+		MsgTypeBean msgTypeBean = MsgTypeService.get(req);
+		// 获取消息类型 
+		String msgType = msgTypeBean.getMsgType();
+		
+		/**
+		 * 1 文本消息
+		 * 2 图片消息
+		 * 3 语音消息
+		 * 4 视频消息
+		 * 5 地理位置消息
+		 * 6 链接消息
+		 * 7 事件 
+		 */
+		switch(msgType){
+			
+			case WechatReqMsgTypeConstant.TEXT:
+				// 1 文本消息
+				break;
+			case WechatReqMsgTypeConstant.IMAGE:
+				// 2 图片消息
+				break;
+			case WechatReqMsgTypeConstant.VOICE:
+				// 3 语音消息
+				break;
+			case WechatReqMsgTypeConstant.VIDEO:
+				// 4 视频消息
+				break;
+			case WechatReqMsgTypeConstant.LOCATION:
+				// 5 地理位置消息
+				break;
+			case WechatReqMsgTypeConstant.LINK:
+				// 6 链接消息
+				break;
+			case WechatReqMsgTypeConstant.EVENT:
+				/**
+				 * 事件消息
+				 * 1 关注/取消关注事件
+				 * 2 扫描带参数二维码事件
+				 * 3 上报地理位置事件
+				 * 4 自定义菜单事件
+				 * 5 点击菜单拉取消息时的事件推送
+				 * 6 点击菜单跳转链接时的事件推送
+				 */
+				String event = msgTypeBean.getEvent();
+				switch (event) {
+				case WechatEventTypeConstant.SUBSCRIBE:
+					logger.info(event);
+					break;
+				case WechatEventTypeConstant.UNSUBSCRIBE:
+					
+					break;
+				case WechatEventTypeConstant.SCAN:
+					
+					break;
+				case WechatEventTypeConstant.LOCATION:
+					
+					break;
+				case WechatEventTypeConstant.CLICK:
+					
+					break;
+				case WechatEventTypeConstant.VIEW:
+					
+					break;
+				default:
+					logger.warn("Wechat Interface Accept UnKnow Event -> " + msgTypeBean.toString());
+					break;
+				}
+				// 跳出外部switch 
+				break;
+			default:
+				// 未知事件消息 
+				logger.warn("Wechat Interface Accept UnKnow MsgType -> " + msgTypeBean.toString());
+		}
+		
+		logger.debug(msgTypeBean.toString());
+		
+		
 		super.doPost(req, resp);
 	}
-
 }
