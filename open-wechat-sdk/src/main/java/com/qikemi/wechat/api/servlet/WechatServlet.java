fb46a781a1b7f9c41355fa23fcd681c8d31cf56f
xiexianbin <a.b@hotmail.com>
2014-10-12 17:29:42 +0800

    complete doGet method

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/servlet/WechatServlet.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/servlet/WechatServlet.java
@@ -2,11 +2,14 @@ package com.qikemi.wechat.api.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 import com.qikemi.wechat.api.service.SignService;
 
@@ -18,26 +21,36 @@ import com.qikemi.wechat.api.service.SignService;
 public class WechatServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1787849341385711015L;
+	private static Logger logger = Logger.getLogger(WechatServlet.class);
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		resp.setContentType("text/html;charset=utf-8");
-		req.setCharacterEncoding("utf-8");
+		try {
+			req.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			logger.error("req编码错误：" + e1.getMessage());
+		}
 		resp.setCharacterEncoding("utf-8");
 
 		// 随机字符串
 		String echostr = req.getParameter("echostr");
-
 		PrintWriter out = null;
-		out = resp.getWriter();
-		// 通过检验signature对请求进行校验，若校验成功则原样返回echostr，否则接入失败
-		if (SignService.checkSignature(req)) {
-			out.print(echostr);
-		}else{
-			out.print("Error Configure.");
+		try {
+			out = resp.getWriter();
+			// 通过检验signature对请求进行校验，若校验成功则原样返回echostr，否则接入失败
+			if (SignService.checkSignature(req)) {
+				out.print(echostr);
+				logger.info("doGet() 配置成功.");
+			} else {
+				out.print("Error Configure.");
+			}
+			out.close();
+		} catch (IOException e) {
+			logger.error("doGet() IOException -->" + e.getMessage());
+		} catch (Exception e) {
+			logger.error("doGet() 参数缺失Exception -->" + e.getMessage());
 		}
-		out.close();
 	}
 
 	@Override
