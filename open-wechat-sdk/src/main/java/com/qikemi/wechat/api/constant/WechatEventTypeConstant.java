4d07e2fb6c613e68033d10db6fb31fb1f9b28b51
xiexianbin <a.b@hotmail.com>
2014-10-12 19:51:34 +0800

    更新

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/constant/WechatEventTypeConstant.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/constant/WechatEventTypeConstant.java
@@ -11,15 +11,31 @@ public class WechatEventTypeConstant {
 	/**
 	 * 事件类型：subscribe(关注)
 	 */
-	public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
+	public static final String SUBSCRIBE = "subscribe";
 	
 	/**
 	 * 事件类型：unSubscribe(取消关注)
 	 */
-	public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
+	public static final String UNSUBSCRIBE = "unsubscribe";
 	
 	/**
-	 * 事件类型：CLICK(自定义菜单点击事件)
+	 * 事件类型：SCAN(用户已关注时的事件推送)
 	 */
-	public static final String EVENT_TYPE_CLICK = "CLICK";
+	public static final String SCAN = "SCAN";
+	
+	/**
+	 * 事件类型：LOCATION(上报地理位置事件)
+	 */
+	public static final String LOCATION = "LOCATION";
+	
+	/**
+	 * 事件类型：CLICK(点击菜单拉取消息时的事件推送)
+	 */
+	public static final String CLICK = "CLICK";
+	
+	/**
+	 * 事件类型：VIEW(点击菜单跳转链接时的事件推送)
+	 */
+	public static final String VIEW = "VIEW";
+	
 }
--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/constant/WechatReqMsgTypeConstant.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/constant/WechatReqMsgTypeConstant.java
@@ -11,30 +11,35 @@ public class WechatReqMsgTypeConstant {
 	/**
 	 * 请求消息类型：文本
 	 */
-	public static final String REQ_MSG_TYPE_TEXT = "text";
+	public static final String TEXT = "text";
 	
 	/**
 	 * 请求消息类型：图片
 	 */
-	public static final String REQ_MSG_TYPE_IMAGE = "image";
+	public static final String IMAGE = "image";
 	
 	/**
 	 * 请求消息类型：链接
 	 */
-	public static final String REQ_MSG_TYPE_LINK = "link";
+	public static final String LINK = "link";
 	
 	/**
 	 * 请求消息类型：地理位置
 	 */
-	public static final String REQ_MSG_TYPE_LOCATION = "location";
+	public static final String LOCATION = "location";
 	
 	/**
 	 * 请求消息类型：音频
 	 */
-	public static final String REQ_MSG_TYPE_VOICE = "voice";
+	public static final String VOICE = "voice";
+	
+	/**
+	 * 请求消息类型：视频 
+	 */
+	public static final String VIDEO = "video";
 	
 	/**
 	 * 请求消息类型：事件
 	 */
-	public static final String REQ_MSG_TYPE_EVENT = "event";
+	public static final String EVENT = "event";
 }
