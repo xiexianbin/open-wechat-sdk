4172e629472f51c352366ad0d5572b44d3958490
xiexianbin <a.b@hotmail.com>
2014-10-13 22:04:43 +0800

    rename

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/entity/message/response/RespTextMessage.java
@@ -0,0 +1,56 @@
+package com.qikemi.wechat.api.entity.message.response;
+
+import com.qikemi.wechat.api.entity.message.RespBaseMessage;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+
+/**
+ * Text Message Bean 
+ * Create Date: 2014年9月27日 下午10:33:15
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class RespTextMessage extends RespBaseMessage {
+	
+	/**
+	 * 回复的消息内容
+	 */
+	@XStreamAlias("Content")
+	private String content;
+
+	/**             constructor                  **/
+	public RespTextMessage() {
+		super();
+	}
+
+	/**
+	 * @param content
+	 */
+	public RespTextMessage(String content) {
+		super();
+		this.content = content;
+	}
+
+	/**
+	 * @param toUserName
+	 * @param fromUserName
+	 * @param createTime
+	 * @param msgType
+	 * @param msgId
+	 * @param content
+	 */
+	public RespTextMessage(String toUserName, String fromUserName, long createTime,
+			String msgType, String content) {
+		super(toUserName, fromUserName, createTime, msgType);
+		this.content = content;
+	}
+
+	/**           getters and setters            **/
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
