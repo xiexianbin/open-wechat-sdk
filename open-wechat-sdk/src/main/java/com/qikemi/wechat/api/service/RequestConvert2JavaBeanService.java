31d9fa2a3a1e9c7dd0495168ed8d9037e1b15f41
xiexianbin <a.b@hotmail.com>
2014-10-16 23:47:46 +0800

    add get image

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/service/RequestConvert2JavaBeanService.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/service/RequestConvert2JavaBeanService.java
@@ -10,6 +10,7 @@ import org.apache.log4j.Logger;
 
 import com.qikemi.packages.xmlJsonAndJavaBean.Xml2JavaBean;
 import com.qikemi.wechat.api.entity.message.MsgTypeBean;
+import com.qikemi.wechat.api.entity.message.request.ReqImageMsg;
 import com.qikemi.wechat.api.entity.message.request.ReqTextMsg;
 
 /**
@@ -54,7 +55,19 @@ public class RequestConvert2JavaBeanService {
 		return (MsgTypeBean) Xml2JavaBean.convert2JavaBean(this.xml, MsgTypeBean.class);
 	}
 	
+	/**
+	 * get Text Message Bean 
+	 * @return
+	 */
 	public ReqTextMsg getTextMessage(){
 		return (ReqTextMsg) Xml2JavaBean.convert2JavaBean(this.xml, ReqTextMsg.class);
+	}
+	
+	/**
+	 * get Image Message Bean
+	 * @return
+	 */
+	public ReqImageMsg getImageMessage() {
+		return (ReqImageMsg) Xml2JavaBean.convert2JavaBean(this.xml, ReqImageMsg.class);
 	}
 }
