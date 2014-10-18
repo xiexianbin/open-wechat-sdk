8c06c5fd04a50d410a1dac41a3f9fc3f57c0bb6a
xiexianbin <a.b@hotmail.com>
2014-10-18 17:53:56 +0800

    add image text bean

---
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/entity/message/response/baseBean/ImgTextBean.java
@@ -0,0 +1,71 @@
+package com.qikemi.wechat.api.entity.message.response.baseBean;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Image-Text message
+ * Create Date: 2014年10月18日 下午5:37:49
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+@XStreamAlias("item")
+public class ImgTextBean {
+
+	@XStreamAlias("Title")
+	private String Title;
+	@XStreamAlias("Description")
+	private String Description;
+	@XStreamAlias("PicUrl")
+	private String PicUrl;
+	@XStreamAlias("Url")
+	private String Url;
+	
+	/**             constructor                  **/
+	public ImgTextBean() {
+	}
+	
+	public ImgTextBean(String title, String description, String picUrl,
+			String url) {
+		Title = title;
+		Description = description;
+		PicUrl = picUrl;
+		Url = url;
+	}
+	
+	/**           getters and setters            **/
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getPicUrl() {
+		return PicUrl;
+	}
+	public void setPicUrl(String picUrl) {
+		PicUrl = picUrl;
+	}
+	public String getUrl() {
+		return Url;
+	}
+	public void setUrl(String url) {
+		Url = url;
+	}
+	
+	@Override
+	public String toString() {
+		return "ImgTextBean [Title=" + Title + ", Description=" + Description
+				+ ", PicUrl=" + PicUrl + ", Url=" + Url + ", getTitle()="
+				+ getTitle() + ", getDescription()=" + getDescription()
+				+ ", getPicUrl()=" + getPicUrl() + ", getUrl()=" + getUrl()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+}
