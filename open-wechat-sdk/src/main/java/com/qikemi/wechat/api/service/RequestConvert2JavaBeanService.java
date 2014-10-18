56c462ba2546458987bdfec5c560cef2e2b46c9c
xiexianbin <a.b@hotmail.com>
2014-10-18 15:48:00 +0800

    update

--- open-wechat-sdk/src/main/java/com/qikemi/wechat/api/service/RequestConvert2JavaBeanService.java
+++ open-wechat-sdk/src/main/java/com/qikemi/wechat/api/service/RequestConvert2JavaBeanService.java
@@ -9,12 +9,9 @@ import java.io.UnsupportedEncodingException;
 import org.apache.log4j.Logger;
 
 import com.qikemi.packages.xmlJsonAndJavaBean.Xml2JavaBean;
-import com.qikemi.wechat.api.entity.message.MsgTypeBean;
-import com.qikemi.wechat.api.entity.message.request.ReqImageMsg;
-import com.qikemi.wechat.api.entity.message.request.ReqTextMsg;
 
 /**
- * get Accept message Convert 2 JavaBean 
+ * Convert Accept message 2 JavaBean 
  * Create Date: 2014年10月12日 下午5:58:32
  * Author XieXianbin<a.b@hotmail.com>
  * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
@@ -48,26 +45,11 @@ public class RequestConvert2JavaBeanService {
 	}
 
 	/**
-	 * get msgTypeBean 
+	 * Convert reponse xml to Java Bean 
+	 * @param o
 	 * @return
 	 */
-	public MsgTypeBean getMsgTypeBean(){
-		return (MsgTypeBean) Xml2JavaBean.convert2JavaBean(this.xml, MsgTypeBean.class);
-	}
-	
-	/**
-	 * get Text Message Bean 
-	 * @return
-	 */
-	public ReqTextMsg getTextMessage(){
-		return (ReqTextMsg) Xml2JavaBean.convert2JavaBean(this.xml, ReqTextMsg.class);
-	}
-	
-	/**
-	 * get Image Message Bean
-	 * @return
-	 */
-	public ReqImageMsg getImageMessage() {
-		return (ReqImageMsg) Xml2JavaBean.convert2JavaBean(this.xml, ReqImageMsg.class);
+	public Object convertRep2JaveBean(Class<?> c) {
+		return Xml2JavaBean.convert2JavaBean(this.xml, c);
 	}
 }
