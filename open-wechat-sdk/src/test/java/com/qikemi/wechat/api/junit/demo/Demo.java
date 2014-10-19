01f42d9ae1f9e69bd18e5b7722b8895d60903698
xiexianbin <a.b@hotmail.com>
2014-10-19 22:59:16 +0800

    update

--- open-wechat-sdk/src/test/java/com/qikemi/wechat/api/junit/demo/Demo.java
+++ open-wechat-sdk/src/test/java/com/qikemi/wechat/api/junit/demo/Demo.java
@@ -1,5 +1,7 @@
 package com.qikemi.wechat.api.junit.demo;
 
+import java.util.UUID;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
@@ -10,5 +12,11 @@ public class Demo {
 	@Test
 	public void test1(){
 		logger.debug("hello wechat");
+	}
+	
+	@Test
+	public void test2(){
+		String s = UUID.randomUUID().toString() + UUID.randomUUID().toString();
+		logger.debug(s.replace("-", ""));
 	}
 }
